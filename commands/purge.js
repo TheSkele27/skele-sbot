46cec91a516ab5b4ef36cf8a53f61638cae1f20b
TheSkele27 <mohamedalaadora@gmail.com>
2018-09-13 15:06:01 -0400

    Commit 7

--- commands/purge.js
+++ commands/purge.js
@@ -1,13 +1,15 @@
 const Discord = require("discord.js");
 
 exports.run = async (client, message, args) => {
-    if(!message.member.roles.some(r=>["Admin", "Mod", "BotAdmin", "Manager", "Council"].includes(r.name)) )
+    /* if(!message.member.roles.some(r=>["Admin", "Mod", "BotAdmin", "Manager", "Council"].includes(r.name)) ) */
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) 
     return message.reply("Sorry, you don't have permissions to use this!");
-
+    
+    message.delete()
     const deleteCount = parseInt(args[0], 10);
     
-    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+    if(!deleteCount || deleteCount < 1 || deleteCount > 10000)
+      return message.reply("Please provide a number between 1 and 10000 for the number of messages to delete");
     
     const fetched = await message.channel.fetchMessages({limit: deleteCount});
     message.channel.bulkDelete(fetched)
---
+++ commands/stats.js
@@ -0,0 +1,22 @@
+const { version } = require("discord.js");
+const Discord = require("discord.js");
+const moment = require("moment");
+require("moment-duration-format");
+
+exports.run = (client, message, args, level) => { // eslint-disable-line no-unused-vars
+  const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
+  const embed = new Discord.RichEmbed()
+    .setAuthor(`${client.user.username}`, `${client.user.avatarURL}`)
+    .setTitle(`STATISTICS`)
+    .setColor(message.member.displayColor)
+    .addField("• Memory Usage", `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, true)
+    .addField("• Uptime", `${duration}`, true)
+    .addField("• Users  ", `${client.users.size.toLocaleString()}`, true)
+    .addField("• Servers ", `${client.guilds.size.toLocaleString()}`, true)
+    .addField("• Channels", `${client.channels.size.toLocaleString()}`, true)
+    .addField("• Discord.js Version", `v${version}`, true)
+    .addField("• Node Version", `${process.version}`, true)
+    .addField("Creator", `TheSkele27`, true)
+    .setFooter(`${client.user.username} | TheSkele27`);
+  message.channel.send(embed);
+};

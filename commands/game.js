8f3906e9d3093b4fffa8a5a0325b9a28da7effd4
TheSkele27 <mohamedalaadora@gmail.com>
2018-09-16 14:56:09 -0400

    Commit 8

--- commands/game.js
+++ commands/game.js
@@ -1,36 +1,60 @@
 const Discord = require("discord.js");
 const status = {
-    online: "Online",
-    idle: "Idle",
-    dnd: "Do Not Disturb",
-    offline: "Offline/Invisible"
-  };
-exports.run = async (client, message, args, level) => {
-    const msg = await message.channel.send("Loading...");
-    const botuser = message.mentions.users.first() ? message.guild.members.get(message.mentions.users.first().id) : message.member;
-let bot;
-if (botuser.user.bot === true) {
-  bot = "Yes";
-} else {
-  bot = "No";
-}
+  online: "Online",
+  idle: "Idle",
+  dnd: "Do Not Disturb",
+  offline: "Offline/Invisible"
+};
+exports.run = async (client, message, args) => {
 
-
-try {  const embed = new Discord.RichEmbed()
-    .setAuthor(botuser.displayName, botuser.user.presence.game.assets.smallImageURL)
-    .setThumbnail(botuser.user.presence.game.assets.largeImageURL)
+  const msg = await message.channel.send("Loading...");
+  const botuser = message.mentions.users.first() ? message.guild.members.get(message.mentions.users.first().id) : message.member;
+  const embed = new Discord.RichEmbed()
     .setColor(botuser.displayColor)
-    .addField("Status", `${status[botuser.user.presence.status]}`, true)
-    .addField("Playing", `${botuser.user.presence.game ? `${botuser.user.presence.game.name}` : "not playing anything."}`, true)
-    .addField("Details", `${botuser.user.presence.game.details}`, true)
-    .addField("State", `${botuser.user.presence.game.state}`, true)
-    .addField("ID", botuser.id, false)
-    .addField("Bot", `${bot}`, false)
-    .addField("Guild", `${bot}`, false)
-    .setTimestamp()
-    msg.edit(embed);
-} catch (err) {
-    msg.edit(`Exception: ` 
-    + err);
+    .setFooter(`${client.user.username} | ID ${botuser.id} | TheSkele27`);
+
+  if (!botuser.user.presence.game) {
+    embed.setAuthor(botuser.displayName),
+      embed.addField("Playing", `This user is not playing anything.`, true);
+  } else {
+    const game = botuser.user.presence.game;
+
+    try {
+      embed.setAuthor(botuser.displayName, game.assets.smallImageURL);
+    } catch (err) {
+      embed.setAuthor(botuser.displayName, "https://cdn.discordapp.com/avatars/460639060851949569/4f545d7d0ee4fb31a411035793c4aef8.png?size=2048");
     }
+    try {
+      embed.setThumbnail(game.assets.largeImageURL);
+    } catch (err) {
+      embed.setThumbnail("https://cdn.discordapp.com/avatars/460639060851949569/4f545d7d0ee4fb31a411035793c4aef8.png?size=2048");
+    }
+    try {
+      embed.addField("Status", `${status[botuser.user.presence.status]}`, true);
+    } catch (err) {
+      embed.addField("Status", "An error occured while getting the user's status.", true);
+    }
+    try {
+      embed.addField("Playing", `${game.name}`, true);
+    } catch (err) {
+      embed.addField("Playing", "An error occured while getting the game's name.", true);
+    }
+    try {
+      embed.addField("Details", `${game.details}`, true)
+    }
+    catch (err) {
+      embed.addField("Details", `No Info`, true)
+    }
+    try {
+      embed.addField("State", `${game.state}`, true);
+    } catch (err) {
+      embed.addField("State", "No state", true);
+    }
+    try {
+      embed.addField("Started", `${game.timestamps.start}`, true);
+    } catch (err) {
+      embed.addField("Started", `None`, true);
+    }
+  }
+  msg.edit(embed);
 };

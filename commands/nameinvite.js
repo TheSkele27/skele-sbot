13600571605ffd1460ee9fcb95f6dc34a3895448
TheSkele27 <mohamedalaadora@gmail.com>
2018-09-25 11:24:25 -0400

    Commit 9

---
+++ commands/nameinvite.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js")
+
+module.exports.run = (client, message, guild, msg) => {
+    const members = message.guild.members.filter(member => member.user.username && /(discord\.(gg|io|me|li)\/.+|discordapp\.com\/invite\/.+)/i.test(member.user.username));
+  return message.channel.send(members.map(member => `\`${member.id}\` ${member.displayName}`).join("\n") || "No one in this server has a Discord invite as their user name.");
+}
+
+  
+  exports.help = {
+    name: 'checkservers',
+    category: 'Moderation',
+    description: 'Returns a list of members with an invite as their username.',
+    usage: 'checkservers'
+  };

2303b49f9eed3f2de45c92edea8a603b31a276e5
TheSkele27 <33150773+TheSkele27@users.noreply.github.com>
2018-10-05 13:54:59 +0300

    Update addrole.js

--- commands/addrole.js
+++ commands/addrole.js
@@ -1,5 +1,3 @@
-const Discord = require("discord.js");
-
 exports.run = async (client, message, args) => {
           if(!message.member.hasPermission("MANAGE_ROLES")) return message.reply("Sorry, I can't do that.");
           let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);

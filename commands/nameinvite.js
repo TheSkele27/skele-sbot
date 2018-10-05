6f3162b0a6f05de9a754e35ad241d1d1380359c1
TheSkele27 <33150773+TheSkele27@users.noreply.github.com>
2018-10-05 13:52:20 +0300

    Update nameinvite.js

--- commands/nameinvite.js
+++ commands/nameinvite.js
@@ -1,5 +1,3 @@
-const Discord = require("discord.js")
-
 module.exports.run = (client, message, guild, msg) => {
     const members = message.guild.members.filter(member => member.user.username && /(discord\.(gg|io|me|li)\/.+|discordapp\.com\/invite\/.+)/i.test(member.user.username));
   return message.channel.send(members.map(member => `\`${member.id}\` ${member.displayName}`).join("\n") || "No one in this server has a Discord invite as their user name.");

209b715fd902ec6c633f338674a8d1e010ed9005
TheSkele27 <33150773+TheSkele27@users.noreply.github.com>
2018-10-05 11:19:22 +0300

    Update noods.js

--- commands/noods.js
+++ commands/noods.js
@@ -1,5 +1,3 @@
-const Discord = require("discord.js");
-
 exports.run = async (client, message, args) => {
     message.channel.send("https://cdn.discordapp.com/attachments/364441210670153733/482253148744974346/unknown.png");
     

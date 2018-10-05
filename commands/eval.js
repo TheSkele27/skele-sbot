2928e6c3d79b809a121f25e5c35c75ecb2282fec
TheSkele27 <33150773+TheSkele27@users.noreply.github.com>
2018-10-05 13:54:08 +0300

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -1,4 +1,31 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
+exports.run = async (client, message, args) => { 
+  const code = args.join(' ');
+  try {
+    let evaled = eval(code);
+    if (typeof evaled !== 'string')
+      evaled = require('util').inspect(evaled, {depth:1});
+    const clean = await client.clean(client, evaled);
+    const embed1 = new Discord.RichEmbed()
+      .setAuthor(client.user.username, client.user.avatarURL)
+      .setTitle('__JAVASCRIPT EVALUATION__')
+      .setDescription(`\`\`\`js\n${clean}\n\`\`\``)
+      .setTimestamp()
+      .setFooter(`${client.user.username} | Requested by ${message.author.username}#${message.author.discriminator}`);
+    message.channel.send(embed1);
+  } catch (err) {
+    const embed2 = new Discord.RichEmbed();
+    embed2.setAuthor(client.user.username, client.user.avatarURL);
+    embed2.setColor('#FF0000');
+    embed2.setTitle('__JAVASCRIPT EVALUATION__');
+    embed2.setDescription(`\`ERROR\` \`\`\`xl\n${await client.clean(client, err)}\n\`\`\``);
+    embed2.setTimestamp();
+    embed2.setFooter(`${client.user.username} | Requested by ${message.author.username}#${message.author.discriminator}`);
+    message.channel.send(embed2);
+  }
+};
+
+/*const Discord = require("discord.js");
 
 exports.run = async (client, message, args) => {
     function clean(text) {
@@ -23,3 +50,4 @@ exports.run = async (client, message, args) => {
           }
         }
       };
+*/

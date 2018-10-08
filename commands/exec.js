a95c1522df360c6bff6b8e2389ac325a8d0a36a8
TheSkele27 <mohamedalaadora@gmail.com>
2018-10-08 23:10:21 +0300

    .

--- commands/exec.js
+++ commands/exec.js
@@ -1,9 +1,9 @@
-const { exec } = require('child_process');
+onst { exec } = require('child_process');
 
-exports.run = async (client, message, args) => {
+exports.run = async (client, msg, args) => {
   if(message.author.id !== client.config.ownerID) return;
   const command = args.join(' ');
-  const outMessage = await message.channel.send(`Running \`${command}\`...`);
+  const outMessage = await msg.channel.send(`Running \`${command}\`...`);
   let stdOut = await doExec(command).catch(data=> outputErr(outMessage, data));
   stdOut = stdOut.substring(0, 1750);
   outMessage.edit(`\`OUTPUT\`
@@ -12,10 +12,10 @@ ${stdOut}
 \`\`\``);
 };
 
-const outputErr = (message, stdData) => {
+const outputErr = (msg, stdData) => {
   const { stdout, stderr } = stdData;
   const message = stdout.concat(`\`\`\`${stderr}\`\`\``);
-  message.edit(message);
+  msg.edit(message);
 };
 
 const doExec = (cmd, opts = {}) => {

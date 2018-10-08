7a7173ad25932ae7a08fe65241fb893a6dc91751
TheSkele27 <mohamedalaadora@gmail.com>
2018-10-08 22:54:38 +0300

    exec

---
+++ commands/exec.js
@@ -0,0 +1,27 @@
+const { exec } = require('child_process');
+
+exports.run = async (client, msg, args) => {
+  const command = args.join(' ');
+  const outMessage = await msg.channel.send(`Running \`${command}\`...`);
+  let stdOut = await doExec(command).catch(data=> outputErr(outMessage, data));
+  stdOut = stdOut.substring(0, 1750);
+  outMessage.edit(`\`OUTPUT\`
+\`\`\`sh
+${stdOut}
+\`\`\``);
+};
+
+const outputErr = (msg, stdData) => {
+  const { stdout, stderr } = stdData;
+  const message = stdout.concat(`\`\`\`${stderr}\`\`\``);
+  msg.edit(message);
+};
+
+const doExec = (cmd, opts = {}) => {
+  return new Promise((resolve, reject) => {
+    exec(cmd, opts, (err, stdout, stderr) => {
+      if (err) return reject({ stdout, stderr });
+      resolve(stdout);
+    });
+  });
+};

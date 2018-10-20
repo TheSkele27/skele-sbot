9ba0656fb78aff48b17f556f121e6e86ccd44c42
TheSkele27 <mohamedalaadora@gmail.com>
2018-10-20 18:13:32 +0300

    exec fix maybe?

--- commands/exec.js
+++ commands/exec.js
@@ -1,8 +1,8 @@
-const { exec } = require('child_process');
+const exec = require('child_process');
 
 exports.run = async (client, message, msg, args) => {
   if(message.author.id !== `213632190557192192`) return;
-  const command = args.join(' ');
+  const command = args.join(" ");
   const outMessage = await msg.channel.send(`Running \`${command}\`...`);
   let stdOut = await doExec(command).catch(data=> outputErr(outMessage, data));
   stdOut = stdOut.substring(0, 1750);

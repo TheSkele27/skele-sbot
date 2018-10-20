a1f8636bdcc08bc47fe2018dac6c19f27ec49f8b
TheSkele27 <mohamedalaadora@gmail.com>
2018-10-20 18:45:18 +0300

    .

--- commands/exec.js
+++ commands/exec.js
@@ -1,8 +1,12 @@
 const { exec } = require('child_process');
 
 exports.run = async (client, message, msg, args) => {
+  if (!message.author.id === 'yourid') {
+    return message.channel.send('You are not authorized to run this command.') 
+    } else {
+    //run your shit
+    }
   const command = args.join(' ');
-  if(message.author.id !== `213632190557192192`) return;
   const outMessage = await msg.channel.send(`Running \`${command}\`...`);
   let stdOut = await doExec(command).catch(data=> outputErr(outMessage, data));
   stdOut = stdOut.substring(0, 1750);

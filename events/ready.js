40f0f35ba812d2752ccbc7084491628ca1d5fcfb
TheSkele27 <mohamedalaadora@gmail.com>
2018-10-20 17:58:48 +0300

    thingy

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,6 @@
 exports.run = (client) => {
     console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-    client.user.setActivity(`Serving ${client.guilds.size} servers`);
+    //client.user.setActivity(`Serving ${client.guilds.size} servers`);
+    client.user.setActivity(`??help | ${client.users.size} users`);
 }
   

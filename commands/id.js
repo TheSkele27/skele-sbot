b8cf36f403391cc75dcdc93d515c8ecb9fc3dd7a
CarRoy <43969981+CarRoy@users.noreply.github.com>
2018-10-25 19:43:21 +0200

    Made the id command regular text instead of embed
    now mobile users can copy it yay
    change it however you want

--- commands/id.js
+++ commands/id.js
@@ -3,8 +3,5 @@ const Discord = require("discord.js")
 module.exports.run = async (client, message, args, level) => {
     const msg = await message.channel.send("Loading...");
     let botuser = message.mentions.users.first() ? message.guild.members.get(message.mentions.users.first().id) : message.member
-    const embed = new Discord.RichEmbed()
-    .addField("ID", botuser.id)
-    .setColor(botuser.displayColor)
-    msg.edit(embed)
+    msg.edit("**ID:**\n"botuser.id)
 }

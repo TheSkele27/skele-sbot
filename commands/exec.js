bdcbe4fa0cfbaefdf5e6d4b047464da91b3b78a0
TheSkele27 <mohamedalaadora@gmail.com>
2018-11-12 15:41:14 +0300

    exec fix? and make serverlist prettier

--- commands/exec.js
+++ commands/exec.js
@@ -1,34 +1,8 @@
-/*const { exec } = require('child_process');
+const { exec } = require('child_process');
 
 exports.run = async (client, message, msg, args) => {
-  if (!message.author.id === '213632190557192192') {
-    return message.channel.send('You are not authorized to run this command.') 
-    } else {
-      const command = args.join(' ');
-      const outMessage = await msg.channel.send(`Running \`${command}\`...`);
-      let stdOut = await doExec(command).catch(data=> outputErr(outMessage, data));
-      stdOut = stdOut.substring(0, 1750);
-      outMessage.edit(`\`OUTPUT\`
-    \`\`\`sh
-    ${stdOut}
-    \`\`\``);
-    };
-    const outputErr = (msg, stdData) => {
-      const { stdout, stderr } = stdData;
-      const message = stdout.concat(`\`\`\`${stderr}\`\`\``);
-      msg.edit(message);
-    };
-    
-    const doExec = (cmd, opts = {}) => {
-      return new Promise((resolve, reject) => {
-        exec(cmd, opts, (err, stdout, stderr) => {
-          if (err) return reject({ stdout, stderr });
-          resolve(stdout);
-        });
-      });
-    };
-}
-  /* const command = args.join(' ');
+  if(message.author.id !== client.config.ownerID) return;
+  const command = args.join(' ');
   const outMessage = await msg.channel.send(`Running \`${command}\`...`);
   let stdOut = await doExec(command).catch(data=> outputErr(outMessage, data));
   stdOut = stdOut.substring(0, 1750);
@@ -37,6 +11,7 @@ exports.run = async (client, message, msg, args) => {
 ${stdOut}
 \`\`\``);
 };
+
 const outputErr = (msg, stdData) => {
   const { stdout, stderr } = stdData;
   const message = stdout.concat(`\`\`\`${stderr}\`\`\``);
@@ -50,4 +25,4 @@ const doExec = (cmd, opts = {}) => {
       resolve(stdout);
     });
   });
-}; */
+};
--- commands/serverlist.js
+++ commands/serverlist.js
@@ -1,12 +1,14 @@
 const Discord = require('discord.js');
 
 exports.run = async (client, message, args) => {
+  let botuser = message.mentions.users.first() ? message.guild.members.get(message.mentions.users.first().id) : message.member
   const guildmap = client.guilds.map(i => `(Name: ${i.name}) | (ID: \`${i.id}\`) | (Members: ${i.members.size})`);
 
   const embed = new Discord.RichEmbed()
     .setAuthor('Guilds List')
     .setDescription(guildmap)
     .setFooter(client.user.username, client.user.avatarURL)
+    .setColor(botuser.displayColor)
     .setTimestamp();
 
   const sendSL = await message.channel.send(embed);

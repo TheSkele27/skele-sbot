85b653ae047461b1b3b5d33eac038da17ab2d40f
TheSkele27 <mohamedalaadora@gmail.com>
2018-11-15 12:35:56 +0300

    New Manager command

---
+++ commands/manager.js
@@ -0,0 +1,28 @@
+const { exec } = require('child_process');
+
+exports.run = async (client, message, msg, args) => {
+  if(message.author.id !== client.config.ownerID) return;
+  const command = `pm2 ${args.join(' ')}`;
+  const outMessage = await msg.channel.send(`Processing \`${args.join(' ')}\`...`);
+  let stdOut = await doExec(command).catch(data=> outputErr(outMessage, data));
+  stdOut = stdOut.substring(0, 1750);
+  outMessage.edit(`\`OUTPUT\`
+\`\`\`sh
+${stdOut}
+\`\`\``);
+};
+
+const outputErr = (msg, stdData) => {
+  const { stdout, stderr } = stdData;
+  const message = stdout.concat(`\`\`\`${stderr}\`\`\``);
+  msg.edit(message);
+};
+
+const doExec = (cmd, opts = {}) => {
+  return new Promise((resolve, reject) => {
+    exec(cmd, opts, (err, stdout, stderr) => {
+      if (err) return reject({ stdout, stderr });
+      resolve(stdout);
+    });
+  });
+};
